2f5ad38c5e0ff1f24a9d91c810b156fe8e888ae5
Junbin Zhang <34506631+wolf-bailang@users.noreply.github.com>
2020-03-25 00:33:32 +0800

    Add files via upload

--- Lab2 Path Planning/PathPlanning/dijkstra.py
+++ Lab2 Path Planning/PathPlanning/dijkstra.py
@@ -15,39 +15,46 @@ class AStar():
 
     # estimation
     def _distance(self, a, b):
-        # Diagonal distance
+        # Diagonal distance h(x)
         d = np.max([np.abs(a[0]-b[0]), np.abs(a[1]-b[1])])
         return d
 
     def planning(self, start=(100,200), goal=(375,520), inter=10, img=None):
         # Initialize 
         self.initialize()
-        self.queue.append(start)
+        self.queue.append(start)    # 把起点加入open list。
         self.parent[start] = None
         self.g[start] = 0
         self.h[start] = self._distance(start, goal)
         node_goal = None
+        # 重复如下过程
         while(1):
             min_dist = 99999
             min_id = -1
+            # 遍历open list ，查找F值最小的节点，把它作为当前要处理的节点。
             for i, node in enumerate(self.queue):
                 # todo
                 #####################################################
                 # In a*  we need to add something in this function
-                f = self.g[node]
+                # f = self.g[node]
+                f = self.g[node] + self.h[node]
                 #####################################################
                 if f < min_dist:
                     min_dist = f
                     min_id = i
 
-            # pop the nearest node 
+            # pop the nearest node
+            # 把这个节点移到close list
             p = self.queue.pop(min_id)
 
             # meet obstacle, skip
+            # 如果它是不可抵达的或者它在 close list 中，忽略它。
             if self.map[p[1],p[0]]<0.5:
                 
                 continue
             # find goal
+            # 停止，当你把终点加入到了 open list 中，此时路径已经找到了，或者查找终点失败，并且 open list 是空的，
+            # 此时没有路径。
             if self._distance(p,goal) < inter:
                 self.goal_node = p
                 break
@@ -58,16 +65,20 @@ class AStar():
             pts_next = pts_next1 + pts_next2
 
             for pn in pts_next:
+                # 如果它不在 open list 中，把它加入 open list ，并且把当前方格设置为它的父亲，记录该方格的 F ， G 和 H 值。
                 if pn not in self.parent:
                     self.queue.append(pn)
                     self.parent[pn] = p
                     self.g[pn] = self.g[p] + inter
                     #todo
+                    #After you find the point, you need to calculate this point's estimation
                     ##############################################
-                    
                     # update the estimation
-
+                    self.h[pn] = self.h[p] + inter
                     ##############################################
+                # 如果它已经在 open list 中，检查这条路径 ( 即经由当前方格到达它那里 ) 是否更好，用 G 值作参考。更小的
+                # G 值表示这是更好的路径。如果是这样，把它的父亲设置为当前方格，并重新计算它的 G 和 F 值。如果你的
+                # open list 是按 F 值排序的话，改变后你可能需要重新排序。
                 elif self.g[pn]>self.g[p] + inter:
                     self.parent[pn] = p
                     self.g[pn] = self.g[p] + inter
